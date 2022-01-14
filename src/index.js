3571481172e79a2ebcf46f2d10270a515ec3b54f
manpreetsinghbhullar <95140136+manpreetsinghbhullar@users.noreply.github.com>
2022-01-14 16:40:06 +0530

    Initial commit
    Signed-off-by: manpreetsinghbhullar <95140136+manpreetsinghbhullar@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Chart from './components/TVChartContainer'
+
+ReactDOM.render(<Chart />, document.getElementById('tv-chart'));
+
